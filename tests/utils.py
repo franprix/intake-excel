Fix tests (floating point precision of Excel files) and missing openpyxl requirement

tests/utils.py
@@ -7,10 +7,13 @@ df = pd.DataFrame({
     'b': np.random.randint(100),
     'c': np.random.choice(['a', 'b', 'c', 'd'], size=100)
 })
-df.index.name = 'p'
+# There is a floating point precision loss when writing a dataframe
+# to an Excel file. To be able to properly test the package we
+# round all floating point values to 6 decimals.
+df.a = df.a.round(6)
 
 
 @pytest.fixture(scope='module')
 def temp_plain_excel():
     filename = 'temp_plain_excel.xlsx'
-    df.to_excel(filename)
+    df.to_excel(filename, float_format='%.6f', index=False)