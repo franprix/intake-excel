0bced53017e53a62c7bf0274fba7c84ec2946d24
Guillaume <gansanay@franprix.fr>
2019-02-27 10:16:19 +0100

    Syntax

--- intake_excel/source.py
+++ intake_excel/source.py
@@ -82,7 +82,7 @@ class ExcelSource(base.DataSource):
         import ssl
         ctx = ssl.create_default_context()
         ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NON
+        ctx.verify_mode = ssl.CERT_NONE
 
         resp = urlopen(urlpath, context=ctx)
         zipfile = ZipFile(BytesIO(resp.read()))
