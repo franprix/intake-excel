4dfc85010fefd911e9011f5af57120640565102d
Guillaume <gansanay@franprix.fr>
2019-02-27 10:55:26 +0100

    Initial .gitignore, fix missing pytest import

--- tests/test_intake_excel.py
+++ tests/test_intake_excel.py
@@ -5,8 +5,8 @@ from .utils import temp_plain_excel, df
 
 def test_fixture(temp_plain_excel):
     d2 = pd.read_excel('temp_plain_excel.xlsx')
-assert df.equals(d2)
+    assert df.equals(d2)
 
 def test_simple(temp_plain_excel):
     d2 = ExcelSource('temp_plain_excel.xlsx').read()
-assert df.equals(d2)
+    assert df.equals(d2)
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pytest
 
 df = pd.DataFrame({
     'a': np.random.rand(100),
